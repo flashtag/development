0b9014c6635744c3702f6e9a46705fec6752bc38
ryanwinchester <fungku@gmail.com>
2015-11-03 16:15:49 -0800

    clean up meta tags

--- app/Client/resources/views/partials/post-meta.blade.php
+++ app/Client/resources/views/partials/post-meta.blade.php
@@ -1,36 +1,43 @@
 @extends ('flashtag')
 
-@section ('html') <html itemscope itemtype="http://schema.org/Article"> @stop
+@section ('html-props') itemscope itemtype="http://schema.org/Article" @stop
 
-@section ('title') {{ $meta->title }} @stop
+@section ('title') {{ $post->title }} @stop
 
 @section ('meta')
     <meta name="description" content="{{ $meta->description }}" />
 
-    <!-- Schema.org markup for Google+ -->
-    <meta itemprop="name" content="{{ $meta->title }}">
+    {{-- Schema.org markup for Google+ --}}
+    <meta itemprop="name" content="{{ $post->title }}">
     <meta itemprop="description" content="{{ $meta->description }}">
     <meta itemprop="image" content="{{ $meta->image }}">
 
-    <!-- Twitter Card data -->
+    {{-- Twitter Card data --}}
     <meta name="twitter:card" content="{{ $meta->twitter_card }}">
     <meta name="twitter:site" content="{{ $meta->twitter_site }}">
-    <meta name="twitter:title" content="{{ $meta->title }}">
+    <meta name="twitter:title" content="{{ $post->title }}">
     <meta name="twitter:description" content="{{ $meta->description }}">
     <meta name="twitter:creator" content="{{ $meta->twitter_creator }}">
     <meta name="twitter:image:src" content="{{ $meta->image }}">
 
-    <!-- Open Graph data -->
-    <meta property="og:title" content="{{ $meta->title }}" />
-    <meta property="og:type" content="{{ $meta->og_type }}" />
+    {{-- Open Graph data for Facebook and Pinterest --}}
+    <meta property="og:site_name" content="{{ $site->name }}" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="{{ $post->title }}" />
     <meta property="og:url" content="{{ $meta->url }}" />
-    <meta property="og:image" content="{{ $meta->image }}" />
     <meta property="og:description" content="{{ $meta->description }}" />
-    <meta property="og:site_name" content="{{ $meta->site_name }}" />
-    <meta property="article:published_time" content="{{ $meta->published_time }}" />
-    <meta property="article:modified_time" content="{{ $meta->modified_time }}" />
-    <meta property="article:section" content="{{ $meta->section }}" />
-    <meta property="article:tag" content="{{ $meta->tag }}" />
-    <meta property="fb:admins" content="{{ $meta->facebook_admins }}" />
-    <meta property="fb:app_id" content="{{ $meta->facebook_app_id }}" />
+    <meta property="og:image" content="{{ $meta->image }}" />
+    <meta property="og:rating" content="{{ $post->getRating() }}">
+    <meta property="og:rating_scale" content="100">
+    <meta property="og:rating_count" content="{{ $post->ratings->count() }}">
+    <meta property="article:published_time" content="{{ $post->start_showing_at ? $post->start_showing_at->toIso8601String() : $post->created_at->toIso8601String() }}" />
+    <meta property="article:modified_time" content="{{ $post->updated_at->toIso8601String() }}" />
+    <meta property="article:section" content="{{ $post->category->title }}" />
+    @foreach ($post->tags as $tag)
+        <meta property="article:tag" content="{{ $tag->name }}" />
+    @endforeach
+    {{-- Facebok-specific --}}
+    @if ($meta->facebook_page_id) <meta property="fb:page_id" content="{{ $meta->facebook_page_id }}" /> @endif
+    @if ($meta->facebook_admins) <meta property="fb:admins" content="{{ $meta->facebook_admins }}" /> @endif
+    @if ($meta->facebook_app_id) <meta property="fb:app_id" content="{{ $meta->facebook_app_id }}" /> @endif
 @stop
