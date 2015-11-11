da6105c5d902ba6f723c8be6c49977dd11fd3830
ryanwinchester <fungku@gmail.com>
2015-11-11 13:09:13 -0800

    why won't the dashboard render?

--- app/Admin/resources/assets/js/components/home.vue
+++ app/Admin/resources/assets/js/components/home.vue
@@ -1,9 +1,13 @@
 <template>
-    <h1>Dashboard</h1>
+    <h1>Dashboard for {{ currentUser.name }}</h1>
+
+    <p>Why won't this show up?</p>
 </template>
 
 <script>
     export default {
-        //
+
+        props: ['current-user']
+
     }
 </script>
