2946f4af95272181b09cdade38e1013335ec65f9
ryanwinchester <fungku@gmail.com>
2015-11-13 11:12:14 -0800

    update welcome page (temp)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">FLASHTAG</div>
             </div>
         </div>
     </body>
