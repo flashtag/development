9fb1d05fc69f67153b50920bb674e35cc5df0078
ryanwinchester <fungku@gmail.com>
2015-11-19 10:41:49 -0800

    still busted ckeditor

--- app/Admin/resources/assets/js/directives/rich-editor.js
+++ app/Admin/resources/assets/js/directives/rich-editor.js
@@ -2,8 +2,6 @@ export default {
 
     twoWay: true,
 
-    params: ['content'],
-
     bind: function () {
         this.vm.$nextTick(this.setupEditor.bind(this));
     },
@@ -11,7 +9,6 @@ export default {
     setUpEditor: function () {
         var self = this;
         CKEDITOR.replace(this.el.id);
-        CKEDITOR.instances[this.el.id].setData(this.params.content);
         CKEDITOR.instances[this.el.id].on('change', function () {
             self.set(CKEDITOR.instances[self.el.id].getData());
         });
@@ -22,7 +19,6 @@ export default {
             return this.vm.$nextTick(this.update.bind(this, value));
         }
         CKEDITOR.instances[this.el.id].setData(value);
-        $(this.el).trigger('change');
     },
 
     unbind: function () {
