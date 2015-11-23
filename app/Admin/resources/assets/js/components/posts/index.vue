31b4fde6a41fdc83be48113358e34563792a6c8a
ryanwinchester <fungku@gmail.com>
2015-11-23 13:36:19 -0800

    reorder posts in posts list if filtering by category

--- app/Admin/resources/assets/js/components/posts/index.vue
+++ app/Admin/resources/assets/js/components/posts/index.vue
@@ -27,6 +27,7 @@
     <table class="Posts table table-striped table-hover">
         <thead>
             <tr>
+                <th v-if="categoryFilter"><a href="#" @click.prevent="sortBy('order')">Order <i :class="orderIcon('order')"></i></a></th>
                 <th><a href="#" @click.prevent="sortBy('title')">Title <i :class="orderIcon('title')"></i></a></th>
                 <th><a href="#" @click.prevent="sortBy('category.data.name')">Category <i :class="orderIcon('category.data.name')"></i></a></th>
                 <th><a href="#" @click.prevent="sortBy('created_at')">Created <i :class="orderIcon('created_at')"></i></a></th>
@@ -40,6 +41,15 @@
                     | filterBy categoryFilter in 'category.data.name'
                     | orderBy sortKey sortDir"
                 class="Post" :class="{ 'Post--unpublished': !post.is_published }">
+
+                <td v-if="categoryFilter" class="order">
+                    <input class="post__order"
+                           type="number"
+                           value="{{ post.order }}"
+                           @keyup.enter="blur"
+                           @focusout="reorder(post, $event)"
+                           number>
+                </td>
 
                 <td>
                     <a href="#!/posts/{{ post.id }}" @click.prevent="goToPost(post)">{{ post.title }}</a>
@@ -92,8 +102,8 @@
                 users: [],
                 titleFilter: null,
                 categoryFilter: null,
-                sortKey: null,
-                sortDir: -1
+                sortKey: 'order',
+                sortDir: 1
             }
         },
 
@@ -222,6 +232,89 @@
                 this.$nextTick(function() {
                     $('[data-toggle="tooltip"]').tooltip();
                 });
+            },
+
+            /**
+             * Reorder the posts' list_positions
+             * @param post
+             * @param e
+             */
+            reorder: function(post, e) {
+                var self = this;
+
+                var new_position = parseInt(e.target.value);
+                // If the position is not a number, we are done here.
+                if (isNaN(new_position)) {
+                    e.target.value = post.order;
+                    return;
+                }
+
+                var max = self.posts.reduce(function(max, post) {
+                    return (post.order > max) ? post.order : max;
+                }, 0);
+
+                new_position = (new_position > 0) ? new_position : 1;
+                new_position = (new_position > max) ? max : new_position;
+
+                // If the position is not different, we are done here.
+                if (new_position === post.order) {
+                    e.target.value = post.order;
+                    return;
+                }
+
+                // Shift all the posts' between the new list position and old list position up or down by one.
+                // If we are moving the list position to a lower number, the other numbers should shift up by one,
+                // but if we are moving the list position to a higher number the others should shift down by one.
+                var increment = (new_position < post.order) ? +1 : -1;
+                var between = (new_position < post.order) ? [new_position, post.order] : [post.order, new_position];
+
+                this.posts = this.posts.map(function (post) {
+                    var p = post.order;
+                    if (p >= between[0] && p <= between[1]) {
+                        post.order += increment;
+                    }
+                    return post;
+                });
+
+                // Save the new position on the current post.
+                post.order = new_position;
+
+                // Persist the new order
+                this.saveOrder(post);
+
+                this.sortKey = 'order';
+
+                setTimeout(function() {
+                    this.scrollTo($(e.target).closest('tr'));
+                }.bind(this), 0);
+            },
+
+            /**
+             * Save the post's position to the database.
+             * @param post
+             */
+            saveOrder: function (post) {
+                client({
+                    method: "PATCH",
+                    path: '/posts/' + post.id + '/reorder',
+                    entity: { order: post.order }
+                });
+            },
+
+            scrollTo: function(target) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 280
+                }, 700);
+
+                target.addClass("pulse").delay(4000).queue(function () {
+                    $(this).removeClass("pulse").dequeue();
+                });
+
+                return false;
+            },
+
+            blur: function(e) {
+                e.target.blur();
             }
 
         },
