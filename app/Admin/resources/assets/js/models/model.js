987027fe772dc86ed17784ef22a49d477ebb6052
ryanwinchester <fungku@gmail.com>
2015-12-11 20:50:28 -0800

    i don't know why it was indented

--- app/Admin/resources/assets/js/models/model.js
+++ app/Admin/resources/assets/js/models/model.js
@@ -1,53 +1,53 @@
-    class Model {
-        constructor(resourcePath, attributes) {
-            this.resourcePath = resourcePath;
-            this.attributes = attributes;
-            this._createGettersAndSetters();
-        }
+class Model {
+    constructor(resourcePath, attributes) {
+        this.resourcePath = resourcePath;
+        this.attributes = attributes;
+        this._createGettersAndSetters();
+    }
 
-        static create(attributes) {
-            var model = new this(attributes);
+    static create(attributes) {
+        var model = new this(attributes);
 
-            return client({
-                method: 'POST',
-                path: '/' + model.resourcePath,
-                entity: model.attributes
-            });
-        }
+        return client({
+            method: 'POST',
+            path: '/' + model.resourcePath,
+            entity: model.attributes
+        });
+    }
 
-        save() {
-            return client({
-                method: 'PUT',
-                path: '/' + this.resourcePath + '/' + this.attributes['id'],
-                entity: this.attributes
-            });
-        }
+    save() {
+        return client({
+            method: 'PUT',
+            path: '/' + this.resourcePath + '/' + this.attributes['id'],
+            entity: this.attributes
+        });
+    }
 
-        update(attributes) {
-            return client({
-                method: 'PATCH',
-                path: '/' + this.resourcePath + '/' + this.attributes['id'],
-                entity: attributes
-            });
-        }
+    update(attributes) {
+        return client({
+            method: 'PATCH',
+            path: '/' + this.resourcePath + '/' + this.attributes['id'],
+            entity: attributes
+        });
+    }
 
-        destroy() {
-            return client({
-                method: 'DELETE',
-                path: '/' + this.resourcePath + '/' + this.attributes['id']
-            })
-        }
+    destroy() {
+        return client({
+            method: 'DELETE',
+            path: '/' + this.resourcePath + '/' + this.attributes['id']
+        })
+    }
 
-        _createGettersAndSetters() {
-            for (let prop in this.attributes) {
-                if (this.attributes.hasOwnProperty(prop)) {
-                    Object.defineProperty(this, prop, {
-                        get: function() { return this.attributes[prop]; },
-                        set: function(value) { this.attributes[prop] = value; }
-                    });
-                }
+    _createGettersAndSetters() {
+        for (let prop in this.attributes) {
+            if (this.attributes.hasOwnProperty(prop)) {
+                Object.defineProperty(this, prop, {
+                    get: function() { return this.attributes[prop]; },
+                    set: function(value) { this.attributes[prop] = value; }
+                });
             }
         }
     }
+}
 
-    export default Model;
+export default Model;
