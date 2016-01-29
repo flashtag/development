b065c57f7e77ab13fedb890be39c77e78973b32f
ryanwinchester <fungku@gmail.com>
2016-01-29 15:23:54 -0800

    Update fields template, agains

--- app/Admin/resources/views/fields/templates/rich_text.blade.php
+++ app/Admin/resources/views/fields/templates/rich_text.blade.php
@@ -3,5 +3,5 @@
 
 <label for="{{ $field->name }}">{{ $field->label }}</label>
 <textarea name="fields[{{$field->id}}]" id="{{ $field->name }}" class="form-control rich-editor">
-    {!! count($post->fields) ? $post->fields->where('id', $field->id)->first()->pivot->value : '' !!}
+    {!! $post_field ? $post_field->pivot->value : '' !!}
 </textarea>
