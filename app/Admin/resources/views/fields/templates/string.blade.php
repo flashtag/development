552c3b4177c56a5825dd807f0869d24b238022cf
ryanwinchester <fungku@gmail.com>
2016-01-29 15:22:22 -0800

    Update fields tempalte

--- app/Admin/resources/views/fields/templates/string.blade.php
+++ app/Admin/resources/views/fields/templates/string.blade.php
@@ -1,4 +1,6 @@
 
+<?php $post_field = $post->fields ? $post->fields->where('id', $field->id)->first() : null; ?>
+
 <label for="{{ $field->name }}">{{ $field->label }}</label>
 <input type="text" name="fields[{{$field->id}}]" id="{{ $field->name }}" class="form-control"
-       value="{{ count($post->fields) ? $post->fields->where('id', $field->id)->first()->pivot->value : '' }}">
+       value="{{ $post_field ? $post_field->pivot->value : '' }}">
