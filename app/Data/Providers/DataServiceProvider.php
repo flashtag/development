643acff81d2142f36172742f9b5493883fd5b9a2
ryanwinchester <fungku@gmail.com>
2016-02-11 21:53:47 -0800

    Update auto-presenter

--- app/Data/Providers/DataServiceProvider.php
+++ app/Data/Providers/DataServiceProvider.php
@@ -24,6 +24,7 @@ class DataServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerBindings();
         $this->registerServiceProviders();
         $this->registerBindings();
     }
