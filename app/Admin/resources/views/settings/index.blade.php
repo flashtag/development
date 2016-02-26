8c60160e1a57dddcc52155a6ee30483233fcdec5
ryanwinchester <fungku@gmail.com>
2016-02-25 16:53:40 -0800

    Add settings to admin menu

--- app/Admin/resources/views/settings/index.blade.php
+++ app/Admin/resources/views/settings/index.blade.php
@@ -1,5 +1,19 @@
 @extends('admin::layout')
 
 @section('content')
-    <settings :current-user="user"></settings>
+    @include('admin::partials.form-errors')
+
+    <div class="panel panel-default">
+        <div class="panel-heading">Settings</div>
+        <div class="panel-body">
+            <div class="form-group">
+                <label for="site_name">Site Name</label>
+                <input type="text" value="{{ settings('site_name') }}" name="site_name" id="site_name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="site_tagline">Site Tagline</label>
+                <input type="text" value="{{ settings('site_tagline') }}" name="site_tagline" id="site_tagline" class="form-control">
+            </div>
+        </div>
+    </div>
 @endsection
