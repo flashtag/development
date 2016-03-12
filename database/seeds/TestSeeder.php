7b8ea8d004d55097c0f63ec5c2db45b226202810
ryanwinchester <fungku@gmail.com>
2016-03-11 22:05:02 -0800

    fix seeder image

--- database/seeds/TestSeeder.php
+++ database/seeds/TestSeeder.php
@@ -215,7 +215,7 @@ class TestSeeder extends Seeder
     <p>Spaceflights cannot be stopped. This is not the work of any one man or even a group of men. It is a historical process which mankind is carrying out in accordance with the natural laws of human development.</p>
     <h2 class="section-heading">Reaching for the Stars</h2>
     <p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart. Seeing this has to change a man.</p>
-    <a href="#"><img class="img-responsive" src="/assets/front/clean-blog/img/post-sample-image.jpg" alt=""></a>
+    <a href="#"><img class="img-responsive" src="/assets/themes/clean-creative/img/post-sample-image.jpg" alt=""></a>
     <span class="caption text-muted">To go places and do things that have never been done before – that’s what living is all about.</span>
     <p>Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.</p>
     <p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore, and this is exploration at its greatest.</p>
