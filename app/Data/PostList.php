a28282c2cca9697581b99c4a6d36f6f9b7dd7ff1
ryanwinchester <fungku@gmail.com>
2016-04-21 12:34:55 -0700

    add get by slug to post list model

--- app/Data/PostList.php
+++ app/Data/PostList.php
@@ -18,6 +18,15 @@ class PostList extends Model
     protected $guarded = ['id', 'updated_at', 'created_at'];
 
     /**
+     * @param string $slug
+     * @return mixed
+     */
+    public static function getBySlug($slug)
+    {
+        return static::with('posts')->where('slug', $slug)->first();
+    }
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function posts()
